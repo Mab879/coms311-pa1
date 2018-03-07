215d1abf515a151b26663b5767d05de6bc6d912a
Joel May <joel@jmay.us>
2018-03-07 10:46:23 -0600

    HashTable documentation

--- src/HashTable.java
+++ src/HashTable.java
@@ -19,43 +19,85 @@ public class HashTable {
     private HashFunction hashFunction;
     private int size;
 
+    /**
+     * Constructs a HashTable with a size of the prime larger and closest to size.
+     * @param size the basis for the table size; used to find a prime
+     */
     public HashTable(int size) {
         this.size = Utils.getPrime(size);
         hashFunction = new HashFunction(size);
     }
 
+    /**
+     * The maximum load of the hash table.
+     * @return the maximum load of the hash table
+     */
     public int maxLoad() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * The average load of the hash table.
+     * @return the average load of the hash table
+     */
     public float averageLoad() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * The current size of the hash table.
+     * @return the current size of the hash table
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * The number of *distinct* tuples stored in the hash table.
+     * @return the number of *distinct* tuples in the hash table
+     */
     public int numElements() {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * The load factor
+     * @return numElements()/size()
+     */
     public float loadFactor() {
         return (float) numElements() / (float) size();
     }
 
+    /**
+     * Adds the tuple to the hash table. May increase the size of the table as necessary.
+     * @param t the tuple to be added.
+     */
     public void add(Tuple t) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Gets the elements whose key equals k.
+     * @param k the key to search for
+     * @return the elements with the key
+     */
     public ArrayList<Tuple> search(int k) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Finds the number of occurrences of the specified tuple.
+     * @param t the tuple to search for
+     * @return the number of occurrences of the tuple
+     */
     public int search(Tuple t) {
         throw new UnsupportedOperationException();
     }
 
+    /**
+     * Removes *one occurrence* of the tuple, if it exists.
+     * @param t the tuple to remove
+     */
     public void remove(Tuple t) {
         throw new UnsupportedOperationException();
     }
