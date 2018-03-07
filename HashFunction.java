99a2cfe1c3ef29b69767b5311df8b434e5f7c8e4
Matthew Burket <m@tthewburket.com>
2018-03-07 09:53:21 -0600

    Fix 311's inability to write well formatted code

--- HashFunction.java
+++ HashFunction.java
@@ -3,7 +3,9 @@
  * the form (ax+b)%p.
  * Integers a and b are chosen randomly from {0,1, ...p-1}
  */
+
 import java.util.*;
+
 public class HashFunction {
 
     private int a, b, p;
@@ -12,29 +14,28 @@ public class HashFunction {
         p = findPrime(range);
         Random r = new Random();
         a = 0;
-        while (a==0)
+        while (a == 0)
             a = r.nextInt(p);
         b = 0;
-        while (b==0)
+        while (b == 0)
             b = r.nextInt(p);
 
     }
 
     /**
-     *
      * @param x integer that will be hashed
      * @return hash value of x
      */
     public int hash(int x) {
         x = (int) Math.abs(x);
-        return (int) Math.abs((a*x + b)%p);
+        return (int) Math.abs((a * x + b) % p);
 
     }
 
     private int findPrime(int n) {
         boolean found = false;
         int num = n;
-        while(!found) {
+        while (!found) {
             if (isPrime(num))
                 return num;
             num++;
@@ -44,8 +45,8 @@ public class HashFunction {
     }
 
     private boolean isPrime(int n) {
-        for(int i= 2; i<=Math.sqrt(n); i++)
-            if (n%i==0)
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n % i == 0)
                 return false;
         return true;
     }
