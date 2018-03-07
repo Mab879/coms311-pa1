4cb41d704fb6e717633232028b2282e81acef6ed
Matthew Burket <m@tthewburket.com>
2018-03-07 16:40:27 -0600

    Added methods for shingles in Brute Force

--- src/BruteForceSimilarity.java
+++ src/BruteForceSimilarity.java
@@ -7,15 +7,31 @@
 // DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
 //  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
 
+import java.util.ArrayList;
+
 /**
- * @author Hugh Potter
+ * @author Matthew Burket
+ * @author Joel May
  */
 
 public class BruteForceSimilarity extends AbstractSimilarity {
-    /** String 1 to compare. */
+    /**
+     * String 1 to compare.
+     */
     private String s1;
-    /** String 2 to compare. */
+    /**
+     * String 2 to compare.
+     */
     private String s2;
+    /**
+     * shingles for s1
+     */
+    ArrayList<String> shinglesS1;
+    /**
+     * shingles for s2
+     */
+    ArrayList<String> shinglesS2;
+
 
     /**
      * Constructor to initialize the values.
@@ -28,6 +44,8 @@ public class BruteForceSimilarity extends AbstractSimilarity {
         super(sLength);
         this.s1 = s1;
         this.s2 = s2;
+        this.shinglesS1 = shingleString(s1);
+        this.shinglesS2 = shingleString(s2);
     }
 
     @Override
@@ -43,5 +61,19 @@ public class BruteForceSimilarity extends AbstractSimilarity {
     @Override
     public float similarity() {
         throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Takes a string and uses the objects sLength to make shingles
+     *
+     * @param s string to shingles
+     * @return An ArrayList of shingles from s
+     */
+    private ArrayList<String> shingleString(String s) {
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 1; i < s.length() - sLength; i++) {
+            result.add(s.substring(i, sLength));
+        }
+        return result;
     }
 }
