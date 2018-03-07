48662b8e13a6e1dac3123646bf68a5a06b723846
Matthew Burket <m@tthewburket.com>
2018-03-07 10:13:06 -0600

    Basics on HashTable

--- Utils.java
+++ Utils.java
@@ -1,5 +1,22 @@
 public class Utils {
-    public static float VectorLength(String s) {
+    public static float vectorLength(String s) {
         throw new UnsupportedOperationException();
+    }
+    public static int getPrime(int num) {
+        while (true) {
+            if (isPrime(num)) {
+                return num;
+            }
+            num++;
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if (i % 2 == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
